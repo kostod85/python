2c5b579284bb5e03f504c1991ffc4ceecfd885ce
Kostya Gurinovich <kostod85@gmail.com>
2022-06-19 22:34:18 +0300

    часы есть немного подсмотренные но есть

--- timer.py
+++ timer.py
@@ -1,36 +1,137 @@
-import datetime
-from http.client import FOUND
-from time import strftime
+from datetime import datetime
+import os
 import time
-import json
 
 from pyrsistent import s
 
+нуль = ["■ □ □ □ □ ■",
+        "■ □ ■ ■ □ ■",
+        "■ □ ■ ■ □ ■",
+        "■ □ ■ ■ □ ■",
+        "■ □ ■ ■ □ ■",
+        "■ □ ■ ■ □ ■",
+        "■ □ □ □ □ ■"]
 
-def read_file():
-    with open("numb.json", "r+", encoding = "utf-8") as file:
-        x = file.read().split(",")[1]
-        file.close()
-        return x
-    return "not FOUND 404"
+один = ["■ ■ ■ □ ■ ■" ,
+        "■ ■ □ □ ■ ■" ,
+        "■ □ ■ □ ■ ■" ,
+        "■ ■ ■ □ ■ ■" ,
+        "■ ■ ■ □ ■ ■" ,
+        "■ ■ ■ □ ■ ■",
+        "■ ■ ■ □ ■ ■"]
+
+два =  ["■ □ □ □ □ ■" ,
+        "□ ■ ■ ■ ■ □" ,
+        "■ ■ ■ ■ ■ □" ,     
+        "■ ■ ■ ■ □ ■" ,   
+        "■ ■ ■ □ ■ ■" ,
+        "■ ■ □ ■ ■ ■" ,   
+        "□ □ □ □ □ □"]
+
+три =  ["■ □ □ □ □ ■" ,
+        "□ ■ ■ ■ ■ □" ,
+        "■ ■ ■ ■ □ ■" ,
+        "■ □ □ □ ■ ■" ,
+        "■ ■ ■ ■ □ ■" ,
+        "□ ■ ■ ■ ■ □",
+        "■ □ □ □ □ ■"]
+
+четыре =   ["■ □ ■ ■ □ ■" ,
+            "■ □ ■ ■ □ ■" ,
+            "■ □ ■ ■ □ ■" ,
+            "■ □ □ □ □ □" ,
+            "■ ■ ■ ■ □ ■" ,
+            "■ ■ ■ ■ □ ■" ,
+            "■ ■ ■ ■ □ ■"]
+
+пять = ["□ □ □ □ □ □" ,
+        "□ ■ ■ ■ ■ ■" ,
+        "□ ■ ■ ■ ■ ■" ,
+        "□ □ □ □ □ □" ,
+        "■ ■ ■ ■ ■ □" ,
+        "□ ■ ■ ■ ■ □" ,
+        "■ □ □ □ □ ■" ]
+
+шесть = ["■ ■ □ □ □ □" ,
+         "■ □ ■ ■ ■ ■" ,
+         "□ ■ ■ ■ ■ ■" ,
+         "□ □ □ □ □ ■" ,
+         "□ ■ ■ ■ ■ □" ,
+         "□ ■ ■ ■ ■ □" , 
+         "■ □ □ □ □ ■"] 
+
+семь = ["□ □ □ □ □ □",
+        "■ ■ ■ ■ ■ □", 
+        "■ ■ ■ ■ □ ■",
+        "■ ■ ■ □ ■ ■",
+        "□ □ □ □ □ ■", 
+        "■ □ ■ ■ ■ ■", 
+        "□ ■ ■ ■ ■ ■" ]
+
+восемь = ["□ □ □ □ □ □",
+          "□ ■ ■ ■ ■ □" ,
+          "□ ■ ■ ■ ■ □",
+          "■ □ □ □ □ ■",
+          "□ ■ ■ ■ ■ □" ,
+          "□ ■ ■ ■ ■ □" ,
+          "□ □ □ □ □ □"]
+
+девять =   ["■ □ □ □ □ ■",
+            "□ ■ ■ ■ ■ □" ,
+            "□ ■ ■ ■ ■ □",
+            "■ □ □ □ □ □",
+            "■ ■ ■ ■ ■ □" ,
+            "■ ■ ■ ■ □ ■" ,
+            "□ □ □ □ ■ ■"]
+
+точки =["■ ■ ■ ■ ■ ■",
+        "■ ■ □ □ ■ ■",
+        "■ ■ □ □ ■ ■",
+        "■ ■ ■ ■ ■ ■",
+        "■ ■ □ □ ■ ■",
+        "■ ■ □ □ ■ ■",
+        "■ ■ ■ ■ ■ ■"]
+
+dct = {'0': нуль,
+       '1': один,
+       '2': два,
+       '3': три,
+       '4': четыре,
+       '5': пять,
+       '6': шесть,
+       '7': семь,
+       '8': восемь,
+       '9': девять,
+       ':': точки}    
     
 
-t = datetime.datetime.now()
-time = str(t)
-print(time)
-i = 1
-#while i > 0:
-    #print(time)
-    #time.sleep(2)
-A = { 'one' : "■", 'two' : "□" }
+while True:
+    string = datetime.now().strftime("%H:%M:%S")
+    watch = []
+    os.system('cls' )
+    for x in string:
+        if x in dct.keys():
+            watch.append(dct[x])
+    print(watch[0][0] + watch[1][0] + watch[2][0] + watch[3][0] + watch[4][0] + watch[5][0] + watch[6][0] + watch[7][0])
+    print(watch[0][1] + watch[1][1] + watch[2][1] + watch[3][1] + watch[4][1] + watch[5][1] + watch[6][1] + watch[7][1])
+    print(watch[0][2] + watch[1][2] + watch[2][2] + watch[3][2] + watch[4][2] + watch[5][2] + watch[6][2] + watch[7][2])
+    print(watch[0][3] + watch[1][3] + watch[2][3] + watch[3][3] + watch[4][3] + watch[5][3] + watch[6][3] + watch[7][3])
+    print(watch[0][4] + watch[1][4] + watch[2][4] + watch[3][4] + watch[4][4] + watch[5][4] + watch[6][4] + watch[7][4])
+    print(watch[0][5] + watch[1][5] + watch[2][5] + watch[3][5] + watch[4][5] + watch[5][5] + watch[6][5] + watch[7][5])
+    print(watch[0][6] + watch[1][6] + watch[2][6] + watch[3][6] + watch[4][6] + watch[5][6] + watch[6][6] + watch[7][6])
+    time.sleep(1)
+     
 
+#print(read_file()[11])
+#sTime = time.time()
+#while True:
+    #print(datetime.datetime.now())
+    #time.sleep(15.0 - ((time.time() - sTime) % 15.0))
+    # #def read_file():
+    #with open("numb.json", "r+", encoding = "utf-8") as file: это я пытался осуществить вывод из файла numb.json
 
-def namb_1():
-    print(A['one'], A['one'], A['one'], A["two"], A['one'], A['one'])
-    print(A['one'], A['one'], A["two"], A['two'], A['one'], A['one'])
-    print(A['one'], A['two'], A['one'], A["two"], A['one'], A['one'])
-    print(A['one'], A['one'], A['one'], A["two"], A['one'], A['one'])
-    print(A['one'], A['one'], A['one'], A["two"], A['one'], A['one'])
-    print(A['one'], A['one'], A['one'], A["two"], A['one'], A['one'])
-  
-print(read_file())
+        #x = file.read().split(",")
+        #file.close()
+        #return x
+    #return "not FOUND 404"
+#h = read_file()
