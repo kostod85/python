d012d6fd06cde7a8132f39eec885560eba26cd6c
Kostya Gurinovich <kostod85@gmail.com>
2022-06-23 16:58:08 +0300

    работает но не так как надо

--- generator.py
+++ generator.py
@@ -1,20 +1,20 @@
 import time
 
+
 def gen_numbers():
     num = 0
     while True:
         yield num
         num +=1
-        if num == 5:
-            num += 1
-        
+        if num // 3:
+            print('Вася')
+        elif num != (num // 3):
+            return num
+
+
 gen = gen_numbers()
-i = 0
-while True :
-    i = i + 1
+
+while True:
     print(next(gen))
-    time.sleep(0)
-    if i == 5:
-        print('Вася')
-    elif i >= 11:
-        break
+    time.sleep(1.5)
+
