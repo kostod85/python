5f6969cd5d43552ebaf50989d029d462341c5639
Kostya Gurinovich <kostod85@gmail.com>
2022-06-26 22:40:48 +0300

    не работает, но должно((((

---
+++ tik_tak_toy.py
@@ -0,0 +1,45 @@
+import pygame
+import sys
+
+pygame.init()
+size_block = 100
+margin = 15
+width = height = size_block * 3 + margin * 4
+
+size_window = (width, height)
+screen = pygame.display.set_mode(size_window)
+pygame.display.set_caption("Крестики нолики")
+
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+white = (255, 255, 255)
+mas = [[0] * 3 for i in range(3)]
+query = 0
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x_mouse, y_mouse = pygame.mouse.get_pos()
+            col = x_mouse // (size_block + margin)
+            row = y_mouse // (size_block + margin)
+            #if query % 2 == 0:
+            mas[row][col] = 'x'
+            #else:
+            #    mas[row][col] = 'o'
+            #query += 1
+
+    for row in range(3):
+        for col in range(3):
+            if mas[row][col] == 'x':
+                color = red
+            #elif mas[row][col] == 'o':
+               # color = green
+            else:
+                color = white
+            x = col * size_block + (col + 1) * margin
+            y = row * size_block + (row + 1) * margin
+            pygame.draw.rect(screen, white, (x, y, size_block, size_block))
+    pygame.display.update()
