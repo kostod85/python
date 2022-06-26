fa85677e343a3ccb1a01c68f3b3b3975b99549d2
Kostya Gurinovich <kostod85@gmail.com>
2022-06-26 23:17:22 +0300

    полностью работает

--- tik_tak_toy.py
+++ tik_tak_toy.py
@@ -1,6 +1,22 @@
 import pygame
 import sys
 
+def check_win(mas, sign):
+    zero = 0
+    for row in mas:
+        zero += row.count(0)
+        if row.count(sign) == 3:
+            return sign
+    for col in range(3):
+        if mas[0][col] == sign and mas[1][col] == sign and mas[2][col] == sign:
+            return sign
+    if mas[0][0] == sign and mas[1][1] == sign and mas[2][0] == sign:
+        return sign
+    if mas[0][2] == sign and mas[1][1] == sign and mas[2][0] == sign:
+        return sign
+    if zero == 0:
+        return 'Ничья'
+    return False
 pygame.init()
 size_block = 100
 margin = 15
@@ -15,7 +31,9 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 white = (255, 255, 255)
 mas = [[0] * 3 for i in range(3)]
-query = 0
+gaymer = 0
+game_over = False
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -26,23 +44,44 @@ while True:
             col = x_mouse // (size_block + margin)
             row = y_mouse // (size_block + margin)
             if mas[row][col] == 0:
-                if query % 2 == 0:
+                if gaymer % 2 == 0:
                     mas[row][col] = 'x'
                 else:
                     mas[row][col] = 'o'
-                query += 1
+                gaymer += 1
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_over = False
+                mas = [[0] * 3 for i in range(3)]
+                gaymer = 0
+                screen.fill(black)
 
-    for row in range(3):
-        for col in range(3):
-            if mas[row][col] == 'x':
-                color = red
-            elif mas[row][col] == 'o':
-                color = green
-            else:
-                color = white
-            x = col * size_block + (col + 1) * margin
-            y = row * size_block + (row + 1) * margin
-            pygame.draw.rect(screen, color, (x, y, size_block, size_block))
-            #if color == red:
-                #pygame.draw.line(screen, white)
+    if not game_over:
+        for row in range(3):
+            for col in range(3):
+                if mas[row][col] == 'x':
+                    color = red
+                elif mas[row][col] == 'o':
+                    color = green
+                else:
+                    color = white
+                x = col * size_block + (col + 1) * margin
+                y = row * size_block + (row + 1) * margin
+                pygame.draw.rect(screen, color, (x, y, size_block, size_block))
+                if color == red:
+                    pygame.draw.line(screen, black, (x+5, y+5), (x+size_block - 5, y+size_block - 5), 5)
+                    pygame.draw.line(screen, black, (x+size_block - 5, y + 5), (x+5, y + size_block - 5), 5)
+                elif color == green:
+                    pygame.draw.circle(screen, black, (x+size_block//2, y + size_block // 2), size_block//2-3, 5)
+    if (gaymer - 1) % 2 == 0:
+        game_over = check_win(mas, 'x')
+    else:
+        game_over = check_win(mas, 'o')
+    if game_over:
+        screen.fill(black)
+        font = pygame.font.SysFont('BOLD', 80)
+        text1 = font.render(game_over, True, white)
+        text_rect = text1.get_rect()
+        text_x = screen.get_width() / 2 - text_rect.width/2
+        text_y = screen.get_height() / 2 - text_rect.height / 2
+        screen.blit(text1, (text_x, text_y))
     pygame.display.update()
